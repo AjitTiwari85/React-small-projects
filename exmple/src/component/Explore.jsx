5c2e0d2be0d04feeb72ece470895192a9226dd79
Rahul Tiwari <at856575@gmail.com>
2024-05-11 17:00:56 +0530

    changes

--- exmple/src/component/Explore.jsx
+++ exmple/src/component/Explore.jsx
@@ -11,6 +11,7 @@ function Explore() {
       try {
         const response = await fetch("https://dummyjson.com/products/");
         const data = await response.json();
+
         setProducts(data.products);
       } catch (error) {
         console.error(error);
@@ -29,16 +30,20 @@ function Explore() {
     <div className="container mx-auto px-4">
       <div className="my-2 ">
         <input
-          className="p-2 w-full bg-slate-200 rounded-t-xl outline-none"
+          className="p-2 w-full bg-slate-200 rounded-t-xl outline-none "
           placeholder="Search"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {filteredProducts.length === 0 ? (
+        {filteredProducts.length === 0 && searchQuery.length != 0 ? (
           <div className="absolute inset-0 flex justify-center items-center">
             <div className="text-3xl text-slate-400">No results found</div>
+          </div>
+        ) : filteredProducts.length === 0 ? (
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="text-3xl text-slate-400">Loading...</div>
           </div>
         ) : (
           filteredProducts.map((product) => (
